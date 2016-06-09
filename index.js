1d6f17e2fcdc2fcc10d149038283fb447841633b
jnf <jeremy.flores@gmail.com>
2016-06-09 13:48:58 -0700

    a sloppy implementation to get a working program

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 //bring in the calculator
 var Calculator = require('./calculator.js');
 var calc = new Calculator(0);
+var prompt = require('prompt');
 
-var answer = calc.addition(10);
-console.log("Math!");
-console.log("0 + 10 is " + answer);
+function collect() {
+  // collect some data from the user
+  prompt.get(['number', 'operation'], function(error, result) {
+    var value = math(result);
+    console.log(value);
+
+    ask_again();
+  });
+}
+
+function ask_again() {
+  prompt.get(["again?"], function(error, result) {
+    var answer = result["again?"];
+    if (answer === "yes") {
+      // do it again!
+      collect();
+    } else {
+      // exit!
+      console.log('bye!');
+      process.exit;
+    }
+  })
+}
+
+function math(result) {
+  // receive a number and an operation
+  var number = Number(result.number),
+      operation = result["operation"];
+
+  // use the `calc` instance to do the math
+  // return the solution? result?
+  return calc[operation](number);
+}
+
+//start prompt - required to be able to capture input
+prompt.start();
+
+// kick off the user interaction
+collect();
